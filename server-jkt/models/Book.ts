bc36c20965d86e890c7fee89fa4ab5188c4aa4d5
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-25 00:04:23 +0700

    add model book

--- server-jkt/models/Book.ts
+++ server-jkt/models/Book.ts
@@ -1,14 +1,19 @@
 
+import sequelize from "database/sequelize";
 import {
   Model,
   Optional,
+  DataTypes
 } from "sequelize";
+
+import BookSample from "./BookSample";
+
 
 // These are all the attributes in the Book model
 interface BookAttributes {
-  id: number;
-  title: string;
-  publisher : string;
+  id: Number;
+  title: String;
+  publisher : String;
 };
 
 // Some attributes are optional in `Book.build` and `Book.create` calls
@@ -17,14 +22,39 @@ interface BookCreationAttributes extends Optional<BookAttributes, "id"> {};
 class Book extends Model<BookAttributes, BookCreationAttributes>
 
   implements BookAttributes {
-  public id!: number; // Note that the `null assertion` `!` is required in strict mode.
-  public title!: string;
-  public publisher! : string;
+  public id!: Number; // Note that the `null assertion` `!` is required in strict mode.
+  public title!: String;
+  public publisher! : String;
 
   // timestamps!
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 
+
 }
 
-export default Book;
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique : true
+    },
+    publisher: {
+      type: DataTypes.STRING,
+    },
+    
+  },
+  {
+    tableName: "books",
+    sequelize, // passing the `sequelize` instance is required
+  }
+);
+
+
+export default Book
