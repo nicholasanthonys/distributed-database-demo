bcbdc9dafe2277e34d70cc4c1d5667130b6e33e8
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-25 00:04:30 +0700

    add model book sample

--- server-jkt/models/BookSample.ts
+++ server-jkt/models/BookSample.ts
@@ -1,28 +1,68 @@
 
+import sequelize from "database/sequelize";
 import {
   Model,
   Optional,
+  HasManyAddAssociationMixin,
+  HasManyCountAssociationsMixin,
+  HasManyCreateAssociationMixin,
+  HasManyGetAssociationsMixin,
+  HasManyHasAssociationMixin,
+  DataTypes,
 } from "sequelize";
+import Book from "./Book";
+import Lend from "./Lend";
+import User from "./User";
+
+
 
 // These are all the attributes in the BookSample model
 interface BookSampleAttributes {
   id: number;
-  title: string;
+  location: string;
+  lendable: boolean;
 };
 
 // Some attributes are optional in `BookSample.build` and `BookSample.create` calls
-interface BookSampleCreationAttributes extends Optional<BookSampleAttributes, "id"> {};
+interface BookSampleCreationAttributes extends Optional<BookSampleAttributes, "id"> { };
 
 class BookSample extends Model<BookSampleAttributes, BookSampleCreationAttributes>
 
   implements BookSampleAttributes {
   public id!: number; // Note that the `null assertion` `!` is required in strict mode.
-  public title!: string;
+  public location!: string;
+  public lendable!: boolean;
 
   // timestamps!
   public readonly createdAt!: Date;
   public readonly updatedAt!: Date;
 
 }
+
+BookSample.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    lendable: {
+      type:  DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+    location: {
+      type: new DataTypes.STRING,
+      allowNull: false
+    }
+
+  },
+  {
+    tableName: "book_samples",
+    sequelize, // passing the `sequelize` instance is required
+  }
+);
+
+
 
 export default BookSample;
