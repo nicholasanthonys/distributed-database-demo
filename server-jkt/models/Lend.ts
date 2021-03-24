61780be3e96d2dc5225ec43c9e5b72a20fbc9e69
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-25 00:04:39 +0700

    add model Lend

--- server-jkt/models/Lend.ts
+++ server-jkt/models/Lend.ts
@@ -1,20 +1,24 @@
 
 import {
+  DataTypes,
   Model,
   Optional,
+  Sequelize
 } from "sequelize";
 
+
+import sequelize from "database/sequelize";
 // These are all the attributes in the Lend model
 interface LendAttributes {
   id: number;
   userId: number;
   bookSampleId: number;
-  returnedAt? : Date;
+  returnedAt?: Date;
 
 };
 
 // Some attributes are optional in `Lend.build` and `Lend.create` calls
-interface LendCreationAttributes extends Optional<LendAttributes, "id"> {};
+interface LendCreationAttributes extends Optional<LendAttributes, "id"> { };
 
 class Lend extends Model<LendAttributes, LendCreationAttributes>
 
@@ -22,7 +26,7 @@ class Lend extends Model<LendAttributes, LendCreationAttributes>
   public id!: number; // Note that the `null assertion` `!` is required in strict mode.
   public userId!: number;
   public bookSampleId!: number;
-  public returnedAt? : Date;
+  public returnedAt?: Date;
 
 
   // timestamps!
@@ -30,5 +34,32 @@ class Lend extends Model<LendAttributes, LendCreationAttributes>
   public readonly updatedAt!: Date;
 
 }
+
+Lend.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+    },
+    bookSampleId: {
+      type: DataTypes.INTEGER,
+    },
+    returnedAt: {
+      type: new DataTypes.INTEGER,
+      allowNull: false,
+      unique: true
+    },
+
+  },
+  {
+    tableName: "lends",
+    sequelize, // passing the `sequelize` instance is required
+  }
+);
+
 
 export default Lend;
