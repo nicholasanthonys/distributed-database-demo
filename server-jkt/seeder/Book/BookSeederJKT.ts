2c526fb375e3bc258d1784d7120755a89ddbe4bb
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-25 09:45:24 +0700

    add example book seeder jakarta

---
+++ server-jkt/seeder/Book/BookSeederJKT.ts
@@ -0,0 +1,34 @@
+const BookSeeder = [
+    {
+        title : "Hello, Habits: A Minimalist's Guide to a Better Life",
+        publisher : "Fumio Sasaki",
+        author : "Penguin Random House",
+        coverUrl : "https://images-na.ssl-images-amazon.com/images/I/41FYi16jzwL._SX327_BO1,204,203,200_.jpg"
+    },
+    {
+        title : "Life After Death: A Novel",
+        publisher : "Sister Souljah",
+        author : "Penguin Random House",
+        coverUrl : "https://images-na.ssl-images-amazon.com/images/I/31uSmElZIBL._SX326_BO1,204,203,200_.jpg"
+    },
+    {
+        title : "The Boy, the Mole, the Fox and the Horse",
+        publisher : "Charlie Mackesy ",
+        author : "Penguin Random House",
+        coverUrl : "https://images-na.ssl-images-amazon.com/images/I/418D9yYGB3L._SX369_BO1,204,203,200_.jpg"
+    },
+    {
+        title : "Everything Will Be Okay: Life Lessons for Young Women",
+        publisher : "Dana Periono",
+        author : "Penguin Random House",
+        coverUrl : "https://images-na.ssl-images-amazon.com/images/I/512pA8FmkVL._SX329_BO1,204,203,200_.jpg"
+    },
+    {
+        title : "The Code Breaker: Jennifer Doudna, Gene Editing, and the Future of the Human Race ",
+        publisher : "Walter Isaacson",
+        author : "Penguin Random House",
+        coverUrl : "https://images-na.ssl-images-amazon.com/images/I/31uSmElZIBL._SX326_BO1,204,203,200_.jpg"
+    }
+]
+
+export default BookSeeder
