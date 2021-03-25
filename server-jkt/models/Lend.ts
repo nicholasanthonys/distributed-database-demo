0ffb26dea881ba7ff1782467cc6009582b0f19cd
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-25 09:44:27 +0700

    remove userId and bookSampleId

--- server-jkt/models/Lend.ts
+++ server-jkt/models/Lend.ts
@@ -11,8 +11,6 @@ import sequelize from "database/sequelize";
 // These are all the attributes in the Lend model
 interface LendAttributes {
   id: number;
-  userId: number;
-  bookSampleId: number;
   returnedAt?: Date;
 
 };
@@ -24,8 +22,6 @@ class Lend extends Model<LendAttributes, LendCreationAttributes>
 
   implements LendAttributes {
   public id!: number; // Note that the `null assertion` `!` is required in strict mode.
-  public userId!: number;
-  public bookSampleId!: number;
   public returnedAt?: Date;
 
 
@@ -42,12 +38,6 @@ Lend.init(
       autoIncrement: true,
       primaryKey: true,
     },
-    userId: {
-      type: DataTypes.INTEGER,
-    },
-    bookSampleId: {
-      type: DataTypes.INTEGER,
-    },
     returnedAt: {
       type: new DataTypes.INTEGER,
       allowNull: false,
@@ -58,6 +48,7 @@ Lend.init(
   {
     tableName: "lends",
     sequelize, // passing the `sequelize` instance is required
+    underscored : true
   }
 );
 
