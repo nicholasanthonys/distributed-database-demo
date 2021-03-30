25eeff47a18ac07d5a1354a5a59d5cf8910ec61f
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-30 09:39:37 +0700

    add site location and site url bdg

--- server-jkt/module.d.ts
+++ server-jkt/module.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
     export interface ProcessEnv {
         DB_URL:string 
         PORT:3000
-        SITE_LOCATION : string
+        SITE_LOCATION : string,
+        SITE_URL_BDG : string
     }
 }
