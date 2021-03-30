623e5472434079172af0c9b41a920bc657d8991f
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-30 09:39:05 +0700

    add api service

---
+++ server-jkt/utils/ApiService.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig } from "axios"
+
+const ApiService = {
+    init(baseUrl: string, authorization: any) {
+        axios.defaults.baseURL = baseUrl
+        axios.defaults.headers = authorization
+        axios.defaults.headers.post['Content-Type'] =
+            'application/json'
+    },
+
+    setHeaderMultipartFormData() {
+        axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+    },
+
+
+    get(resource: string, params: any) {
+        return axios.get(`${resource}`, params)
+    },
+
+    post(resource: string, params: any,) {
+        return axios.post(`${resource}`, params)
+    },
+
+
+    put(resource: string, params: any) {
+        return axios.put(`${resource}`, params)
+    },
+
+    delete(resource: string, params: any) {
+        return axios.delete(resource, params);
+    }
+}
+
+export default ApiService
