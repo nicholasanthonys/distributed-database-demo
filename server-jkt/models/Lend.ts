c2c7dc01bfd74efe01fce84da06fbc7e12942f0d
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-30 17:28:56 +0700

    fix data type, add user id to lend model

--- server-jkt/models/Lend.ts
+++ server-jkt/models/Lend.ts
@@ -8,12 +8,14 @@ import {
 
 
 import sequelize from "database/sequelize";
+import BookSample from "./BookSample";
+import User from "./User";
 // These are all the attributes in the Lend model
 interface LendAttributes {
   id: number;
   returnedAt?: Date;
-  bookSampleId : string;
-
+  bookSampleId: string;
+  userId : number
 };
 
 // Some attributes are optional in `Lend.build` and `Lend.create` calls
@@ -24,8 +26,8 @@ class Lend extends Model<LendAttributes, LendCreationAttributes>
   implements LendAttributes {
   public id!: number; // Note that the `null assertion` `!` is required in strict mode.
   public returnedAt?: Date;
-  public bookSampleId! : string;
-
+  public bookSampleId!: string;
+  public userId!  : number;
 
   // timestamps!
   public readonly createdAt!: Date;
@@ -41,21 +43,28 @@ Lend.init(
       primaryKey: true,
     },
     returnedAt: {
-      type: new DataTypes.INTEGER,
-      allowNull: false,
+      type: new DataTypes.DATE,
+      allowNull: true,
     },
-    bookSampleId : {
-        type : DataTypes.UUID,
-        allowNull : false,
+    bookSampleId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    } ,
+    userId : {
+      type : DataTypes.INTEGER,
+      allowNull : false
     }
 
   },
   {
     tableName: "lends",
     sequelize, // passing the `sequelize` instance is required
-    underscored : true
+    underscored: true
   }
 );
 
+
+User.belongsToMany(BookSample, { through: Lend })
+BookSample.belongsToMany(User, { through: Lend })
 
 export default Lend;
