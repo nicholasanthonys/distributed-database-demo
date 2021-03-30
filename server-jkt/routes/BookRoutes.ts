f64b3290ef7b0ab690239ba848458406c43e41fd
nicholasanthonys <anthony.nicholas84@gmail.com>
2021-03-30 11:34:21 +0700

    fix book sample route from book route

--- server-jkt/routes/BookRoutes.ts
+++ server-jkt/routes/BookRoutes.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from 'express'
 import Book from 'models/Book';
-import { where } from 'sequelize';
+import BookSample from 'models/BookSample';
+import ApiService from 'utils/ApiService';
 
 var bookRouter = Router();
 
@@ -36,6 +37,7 @@ bookRouter.put('/', async (req: Request, res: Response) => {
          where: {
             id: Number(id),
          },
+         returning: true,
 
       })
 
@@ -70,11 +72,11 @@ bookRouter.get('/', async (req: Request, res: Response) => {
 })
 
 bookRouter.get('/:id', async (req: Request, res: Response) => {
-   const id = req.params.id as String
+   const id = req.params.id as string
    try {
       let book = await Book.findOne({
          where: {
-            id: Number(id)
+            id
          }
       })
       if (book) {
@@ -82,7 +84,7 @@ bookRouter.get('/:id', async (req: Request, res: Response) => {
       }
 
       return res.status(400).send({
-         message : "Not found"
+         message: "Not found"
       });
    } catch (error) {
       console.log(error)
@@ -91,6 +93,8 @@ bookRouter.get('/:id', async (req: Request, res: Response) => {
       })
    }
 })
+
+
 
 bookRouter.delete('/:id', async (req: Request, res: Response) => {
    const id = req.params.id as String
@@ -111,6 +115,191 @@ bookRouter.delete('/:id', async (req: Request, res: Response) => {
    }
 })
 
+bookRouter.get('/book-sample/:id', async (req: Request, res: Response) => {
+   let bookSampleId = req.params.id as string;
+   try {
+      // get from jakarta 
+      let bookSampleJkt = await BookSample.findByPk(bookSampleId)
+      if (!bookSampleJkt) {
+         ApiService.init(process.env.SITE_URL_BDG, req.headers.authorization)
+         let response = await ApiService.get(`/book-sample/${bookSampleId}`, {});
+         return res.send(response.data)
+      }
+      return res.send(bookSampleJkt)
+
+   } catch (error) {
+      ;
+      return res.send({
+         message: error
+      })
+   }
+})
+
+bookRouter.get('/book-sample', async (req: Request, res: Response) => {
+   const bookId = req.query.bookId;
+
+   var bookSampleJKT: Array<BookSample> = [];
+   var bookSampleBDG: Array<BookSample> = [];
+
+   try {
+      bookSampleJKT = await BookSample.findAll({
+         where: {
+            bookId
+         }
+      })
+
+      // request to site bandung
+      ApiService.init(process.env.SITE_URL_BDG, req.headers.authorization);
+      let response = await ApiService.get('/book-sample', {
+         bookId
+      })
+      bookSampleBDG = response.data;
+
+      // join result bandung and jakarta
+      let result = [...bookSampleJKT, ...bookSampleBDG]
+      return res.send(result);
+   } catch (error) {
+      return res.status(500).send({
+         message: error
+      })
+   }
+})
+
+bookRouter.post('/:id/book-sample', async (req: Request, res: Response) => {
+   let book = await Book.findOne({
+      where: {
+         id: req.params.id
+      }
+   });
+
+   if (book) {
+      const { location, lendable }: { location: string, lendable: boolean, bookId: number } = req.body
+      try {
+         if (location == "jakarta") {
+            let bookSample = await BookSample.create({
+               lendable,
+               location,
+               bookId: book.id
+            })
+            await book.addBookSample(bookSample)
+            let bookWithBookSample = await Book.findOne({
+               where: {
+                  id: book.id
+               },
+               include: BookSample
+            });
+            return res.send(bookWithBookSample);
+         }
+
+         await ApiService.post('/book-sample', {
+            lendable,
+            location,
+            bookId: book.id
+         })
+
+      } catch (error) {
+         return res.status(500).send({
+            message: error
+         })
+      }
+
+   }
+
+   return res.status(400).send({
+      message: "Book Not Found."
+   })
+
+});
+
+
+bookRouter.put('/:id/book-sample', async (req: Request, res: Response) => {
+
+   let book = await Book.findOne({
+      where: {
+         id: req.params.id
+      }
+   });
+
+   if (book) {
+      const { id, location, lendable, bookId }: { id: number, location: string, lendable: boolean, bookId: number } = req.body
+      try {
+         if (location == "jakarta") {
+            await BookSample.update({
+               lendable,
+               bookId
+            }, {
+               where: {
+                  id
+               }
+            });
+
+            let bookWithBookSample = await Book.findOne({
+               where: {
+                  id: book.id
+               },
+               include: BookSample
+            });
+            return res.send(bookWithBookSample);
+         }
+
+         if (location == "bandung") {
+            await ApiService.put('/book-sample', {
+               id,
+               lendable,
+               bookId: book.id
+            })
+         }
+
+      } catch (error) {
+         return res.status(500).send({
+            message: error
+         })
+      }
+
+   }
+
+   return res.status(400).send({
+      message: "Book Not Found."
+   })
+
+
+});
+
+bookRouter.delete('/:idBook/book-sample/:idBookSample', async (req: Request, res: Response) => {
+   const id = req.params.idBook as string
+   const idBookSample = req.params.idBookSample
+
+   let book = await Book.findOne({
+      where: {
+         id
+      }
+   })
+   if (!book) {
+      return res.status(400).send({
+         message: "No book found"
+      })
+   }
+   try {
+      // delete from site jakarta if exist
+      await BookSample.destroy({
+         where: {
+            id: idBookSample
+         }
+      })
+
+      // delete form site bandung
+      await ApiService.delete(`/book-sample/${idBookSample}`, {})
+      return res.status(200).send({
+         message: "Delete success"
+      });
+
+   } catch (error) {
+      return res.status(500).send({
+         message: error
+      });
+   }
+
+});
 
 export default bookRouter;
 
